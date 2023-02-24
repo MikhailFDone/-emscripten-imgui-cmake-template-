1688735fe19ccd18a0fa1f9677d9293fc82d290a
mikhail <mikhail.shirokov@phystech.edu>
2023-02-24 22:26:07 +0300

    fix compilation to html

--- cmake/DearImGui.cmake
+++ cmake/DearImGui.cmake
@@ -1,31 +1,31 @@
 add_library(imgui STATIC
-    "./dependencies/imgui.h"
-    "./dependencies/imgui_internal.h"
-    "./dependencies/imstb_rectpack.h"
-    "./dependencies/imstb_textedit.h"
-    "./dependencies/imstb_truetype.h"
+    "./imgui/imgui.h"
+    "./imgui/imgui_internal.h"
+    "./imgui/imstb_rectpack.h"
+    "./imgui/imstb_textedit.h"
+    "./imgui/imstb_truetype.h"
 
-    "./dependencies/imgui.cpp"
-    "./dependencies/imgui_draw.cpp"
-    "./dependencies/imgui_tables.cpp"
-    "./dependencies/imgui_widgets.cpp"
+    "./imgui/imgui.cpp"
+    "./imgui/imgui_draw.cpp"
+    "./imgui/imgui_tables.cpp"
+    "./imgui/imgui_widgets.cpp"
 
-    "./dependencies/backends/imgui_impl_sdl2.h"
-    "./dependencies/backends/imgui_impl_opengl3.h"
+    "./imgui/backends/imgui_impl_sdl2.h"
+    "./imgui/backends/imgui_impl_opengl3.h"
 
-    "./dependencies/backends/imgui_impl_sdl2.cpp"
-    "./dependencies/backends/imgui_impl_opengl3.cpp"
+    "./imgui/backends/imgui_impl_sdl2.cpp"
+    "./imgui/backends/imgui_impl_opengl3.cpp"
 )
 add_library(imgui::imgui ALIAS imgui)
 
 target_compile_options(imgui PUBLIC "-sUSE_SDL=2;-sUSE_FREETYPE=1")
 
 target_include_directories(imgui SYSTEM PUBLIC
-    "./dependencies"
+    "./imgui"
 )
 
 set_target_properties(imgui PROPERTIES FOLDER dependencies)
 
 # Make all IMGUI files visible
-file(GLOB_RECURSE imgui_sources CONFIGURE_DEPENDS ./dependencies/*.hpp ./dependencies/*.h ./dependencies/*.c ./dependencies/*.cpp)
-source_group(TREE ./dependencies FILES ${imgui_sources})
+file(GLOB_RECURSE imgui_sources CONFIGURE_DEPENDS ./imgui/*.hpp ./imgui/*.h ./imgui/*.c ./imgui/*.cpp)
+source_group(TREE ./imgui FILES ${imgui_sources})
